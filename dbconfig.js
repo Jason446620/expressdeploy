087313d2e0c0deee7bac4dc61d63223c377048cc
Jason446620 <panshubei@outlook.com>
2020-07-07 16:01:52 +0800

    init

---
+++ dbconfig.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+
+    /**Declaration of databases for my development environment**/
+      "development": {
+          "databases": {
+              "dbinmysql": {
+                  "database": "mysql", //you should always save these values in environment variables
+                  "username": "panshubei@panshubeimysql",  //only for testing purposes you can also define the values here
+                  "password":  "Jason.446620",
+                  "host": "panshubeimysql.mysql.database.azure.com",
+                  "port": 3306,
+                  "ssl":true,
+                  "dialect": "mysql",  //here you need to define the dialect of your databse, in my case it is Postgres
+                  "dialectOptions": {
+                    ssl: {
+                        ca: fs.readFileSync(path.resolve(__dirname, 'BaltimoreCyberTrustRoot.crt.pem'))
+                      }
+                  },
+              },
+              "dbinsqlserver": {
+                  "database": "pa*****db", 
+                  "username": "pa***i",  
+                  "password":  "J*****0",
+                  "host": "***sqlserver.database.windows.net",
+                  "port": 1433,
+                  "dialect": "mssql",  //second database can have a different dialect
+                  "dialectOptions": {
+                    options: {
+                        encrypt: true,
+                    }
+                }
+              },
+          },
+      }
+  }
